36bd6f4cb0715f368bfae479860d4b3662093b22
Bjoern Kasper <bjoern.kasper@versanetonline.de>
2020-03-10 15:39:17 +0100

    added config settings for internal LED + upload settings

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,28 @@
+/*
+ * Blink
+ * Turns on an LED on for one second,
+ * then off for one second, repeatedly.
+ */
+
 #include <Arduino.h>
 
+#include "config.hpp"
+
 void setup() {
-  // put your setup code here, to run once:
+  // initialize LED digital pin as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // turn the LED on (HIGH is the voltage level)
+  digitalWrite(LED_BUILTIN, HIGH);
+  // wait for a second
+  delay(1000);
+  // turn the LED off by making the voltage LOW
+  digitalWrite(LED_BUILTIN, LOW);
+   // wait for a second
+  delay(1000);
 }
+
+
+//
